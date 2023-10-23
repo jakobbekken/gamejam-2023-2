79f4636ff289a0a79d5db8b3b6172f43bc8a7645
Jakob Moen Bekken <jakobmb@uia.no>
2023-10-24 00:35:27 +0200

    added bar

--- src/main.rs
+++ src/main.rs
@@ -3,21 +3,30 @@ use bevy::{
     prelude::*,
 };
 
-const CURRENT_FONT_SIZE: f32 = 40.0;
-const TYPED_FONT_COLOR: Color = Color::rgb(1.0, 0.5, 0.5);
-const CURRENT_FONT_COLOR: Color = Color::rgb(0.0, 1.0, 0.5);
-const TEXT_PADDING: Val = Val::Px(5.0);
+use rand::Rng;
+
+const CURRENT_FONT_SIZE: f32 = 50.0;
+const NEXT_FONT_SIZE: f32 = 35.0;
+const TYPED_FONT_COLOR: Color = Color::rgb(0.2, 0.5, 0.0);
+const CURRENT_FONT_COLOR: Color = Color::rgb(1.0, 0.0, 0.5);
+const NEXT_FONT_COLOR: Color = Color::rgb(0.5, 0.5, 1.0);
+const TEXT_PADDING: Val = Val::Px(240.0);
+const NEXT_PADDING: Val = Val::Px(205.0);
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .insert_resource(WordTyping {
-            current: "testing".to_string(),
-            next: "test2".to_string(),
-            input: "".to_string(),
+            current: get_random_word(),
+            next: get_random_word(),
+            typed: 0,
         })
-        .add_systems(Update, (bevy::window::close_on_esc, typing, update_text))
+        .add_systems(
+            Update,
+            (bevy::window::close_on_esc, typing, update_text, update_next),
+        )
+        .add_systems(FixedUpdate, (timer))
         .add_systems(Startup, setup)
         .run();
 }
@@ -26,32 +35,131 @@ fn main() {
 struct WordTyping {
     current: String,
     next: String,
-    input: String,
+    typed: usize,
+}
+
+#[derive(Component)]
+struct CurrentText;
+
+#[derive(Component)]
+struct NextText;
+
+#[derive(Component)]
+struct Durek;
+
+#[derive(Component)]
+struct Bubble;
+
+#[derive(Component)]
+struct GrowingRect;
+
+fn get_random_word() -> String {
+    let words: Vec<&str> = include_str!("words.txt").lines().collect();
+    let mut rng = rand::thread_rng();
+    let random_index = rng.gen_range(0..words.len());
+
+    words[random_index].to_string()
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2dBundle::default());
 
+    // images
+
+    commands.spawn(SpriteBundle {
+        texture: asset_server.load("textures/bg2.png"),
+        ..Default::default()
+    });
+
+    // durek
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("textures/durek.png"),
+            transform: Transform {
+                translation: Vec3::new(400.0, -200.0, 100.0),
+                ..default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(400.0, 400.0)),
+                ..default()
+            },
+            ..default()
+        },
+        Durek,
+    ));
+
+    // bubble
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("textures/bubble.png"),
+            transform: Transform {
+                translation: Vec3::new(100.0, 0.0, 100.0),
+                ..default()
+            },
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(450.0, 350.0)),
+                ..default()
+            },
+            ..default()
+        },
+        Bubble,
+    ));
+
+    commands.spawn((
+        SpriteBundle {
+            transform: Transform {
+                translation: Vec3::new(100.0, -200.0, 200.0),
+                ..default()
+            },
+            sprite: Sprite {
+                color: Color::rgb(1.0, 0.5, 0.5),
+                custom_size: Some(Vec2 { x: 50.0, y: 150.0 }),
+                ..default()
+            },
+            ..default()
+        },
+        GrowingRect,
+    ));
+
     // text
     {
-        commands.spawn((TextBundle::from_sections([
-            TextSection::from_style(TextStyle {
-                font_size: CURRENT_FONT_SIZE,
-                color: TYPED_FONT_COLOR,
+        commands.spawn((
+            TextBundle::from_sections([
+                TextSection::from_style(TextStyle {
+                    font_size: CURRENT_FONT_SIZE,
+                    color: TYPED_FONT_COLOR,
+                    ..default()
+                }),
+                TextSection::from_style(TextStyle {
+                    font_size: CURRENT_FONT_SIZE,
+                    color: CURRENT_FONT_COLOR,
+                    ..default()
+                }),
+            ])
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                top: Val::Px(300.0),
+                left: Val::Px(550.0),
                 ..default()
             }),
-            TextSection::from_style(TextStyle {
-                font_size: CURRENT_FONT_SIZE,
-                color: CURRENT_FONT_COLOR,
+            CurrentText,
+        ));
+
+        commands.spawn((
+            TextBundle::from_sections([TextSection::from_style(TextStyle {
+                font_size: NEXT_FONT_SIZE,
+                color: NEXT_FONT_COLOR,
+                ..default()
+            })])
+            .with_style(Style {
+                position_type: PositionType::Absolute,
+                align_items: AlignItems::Center,
+                top: Val::Px(265.0),
+                left: Val::Px(550.0),
                 ..default()
             }),
-        ])
-        .with_style(Style {
-            position_type: PositionType::Absolute,
-            top: TEXT_PADDING,
-            left: TEXT_PADDING,
-            ..default()
-        }),));
+            NextText,
+        ));
     }
 }
 
@@ -87,52 +195,86 @@ fn key_code_to_char(key_code: KeyCode) -> Option<char> {
     }
 }
 
+fn is_char_equal(char_to_compare: char, input: &str, n: usize) -> bool {
+    if let Some(nth_character) = input.chars().nth(n) {
+        char_to_compare == nth_character
+    } else {
+        false
+    }
+}
+
 fn typing(
     mut commands: Commands,
     mut word_typing: ResMut<WordTyping>,
-    input: Res<Input<KeyCode>>,
     mut kb_input_events: EventReader<KeyboardInput>,
 ) {
     for event in kb_input_events.iter() {
         if let Some(key_code) = event.key_code {
             if event.state == ButtonState::Pressed {
                 if let Some(key_char) = key_code_to_char(key_code) {
-                    word_typing.input.push(key_char);
-                } else if key_code == KeyCode::Back {
-                    word_typing.input.pop();
+                    if is_char_equal(key_char, &word_typing.current, word_typing.typed) {
+                        word_typing.typed += 1;
+                    }
+                    // word_typing.input.push(key_char);
                 }
             }
         }
     }
 }
 
-fn update_text(word_typing: Res<WordTyping>, mut query: Query<&mut Text>) {
+fn update_next(mut query: Query<&mut Text, With<NextText>>, word_typing: Res<WordTyping>) {
     let mut text = query.single_mut();
+    text.sections[0].value = word_typing.next.clone();
+}
 
+fn update_text(
+    mut word_typing: ResMut<WordTyping>,
+    mut current_query: Query<&mut Text, &CurrentText>,
+    time: Res<Time>,
+    mut query: Query<(&GrowingRect, &mut Sprite, &mut Transform)>,
+) {
+    let mut current_text = current_query.single_mut();
     let current_string = word_typing.current.clone();
-    let input_string = word_typing.input.clone();
+    let index = word_typing.typed;
 
-    let current_chars: Vec<char> = current_string.chars().collect();
-    let input_chars: Vec<char> = input_string.chars().collect();
+    if !(index >= current_string.len()) {
+        current_text.sections[0].value = current_string[..index].to_string();
+        current_text.sections[1].value = current_string[index..].to_string();
+    } else {
+        word_typing.current = word_typing.next.clone();
+        word_typing.next = get_random_word();
+        word_typing.typed = 0;
 
-    let mut index = 0;
-
-    for (cc, ic) in current_chars.iter().zip(input_chars.iter()) {
-        if cc == ic {
-            index += 1;
-        } else {
-            break;
+        for (_tag, mut sprite, mut transform) in query.iter_mut() {
+            if let Some(vec) = &sprite.custom_size {
+                println!("The y value is: {}", vec.y);
+                sprite.custom_size = Some(Vec2 {
+                    x: vec.x,
+                    y: vec.y + 1600.0 * time.delta_seconds(),
+                });
+                transform.translation.y += 1600.0 * time.delta_seconds() / 2.0;
+            } else {
+                println!("The value is None");
+            }
         }
     }
+}
 
-    let mut typed = "".to_string();
-    let mut remaining = word_typing.current.clone();
+fn timer(time: Res<Time>, mut query: Query<(&GrowingRect, &mut Sprite, &mut Transform)>) {
+    for (_tag, mut sprite, mut transform) in query.iter_mut() {
+        if let Some(vec) = &sprite.custom_size {
+            println!("The y value is: {}", vec.y);
 
-    if index != 0 {
-        typed = current_string[..index].to_string();
-        remaining = current_string[index..].to_string();
+            let dt = 10.0 * time.delta_seconds();
+            if vec.y >= 10.0 {
+                sprite.custom_size = Some(Vec2 {
+                    x: vec.x,
+                    y: vec.y - dt,
+                });
+                transform.translation.y -= dt / 2.0;
+            }
+        } else {
+            println!("The value is None");
+        }
     }
-
-    text.sections[0].value = typed;
-    text.sections[1].value = remaining;
 }
